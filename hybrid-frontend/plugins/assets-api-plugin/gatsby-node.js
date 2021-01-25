844e4bf448c50df9c304eb5805edd0f360e80a81
Jamie Howells <jamie.howells@unmind.com>
2021-01-25 17:28:12 +0000

    GH-4 Add Gatsby plugin to retrieve list of funds
    * retrieves funds at build time
    * lists funds on index page
    * some initial typings

---
+++ hybrid-frontend/plugins/assets-api-plugin/gatsby-node.js
@@ -0,0 +1,41 @@
+const fetch = require("node-fetch");
+
+const API_BASE_URL = "https://online.bestinvest.co.uk/api/Assets";
+const ASSET_NODE_TYPE = `Asset`;
+
+// print a success message if the plugin loads
+exports.onPreInit = () =>
+  console.log("\x1b[32m%s\x1b", "success loaded assets-api-plugin");
+
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+}) => {
+  const { createNode } = actions;
+
+  const response = await fetch(`${API_BASE_URL}/readymade/standingdata`);
+
+  if (!response.ok) {
+    console.error("🚨 Unable to fetch assets API data");
+    return;
+  }
+
+  const resultData = await response.json();
+
+  resultData.tilneyStandingData.forEach((asset) =>
+    createNode({
+      ...asset,
+      id: createNodeId(`${ASSET_NODE_TYPE}-${asset.id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: ASSET_NODE_TYPE,
+        content: JSON.stringify(asset),
+        contentDigest: createContentDigest(asset),
+      },
+    })
+  );
+
+  return;
+};
