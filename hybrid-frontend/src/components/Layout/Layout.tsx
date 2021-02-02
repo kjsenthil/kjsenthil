f2ac120b38e4eb7a3d45b8f2ec5d0a7fe02b21a6
Jamie Howells <jamie.howells@unmind.com>
2021-02-02 11:44:23 +0000

    GH-16 Add initial theme

--- hybrid-frontend/src/components/Layout/Layout.tsx
+++ hybrid-frontend/src/components/Layout/Layout.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import theme from '../../material-ui/theme';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <Grid container spacing={3}>
-    <Grid item xs={12}>
-      {children}
+  <ThemeProvider theme={theme}>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        {children}
+      </Grid>
     </Grid>
-  </Grid>
+  </ThemeProvider>
 );
 
 export default Layout;
---
+++ hybrid-frontend/src/material-ui/theme.ts
@@ -0,0 +1,22 @@
+import { createMuiTheme, colors } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: colors.common.white,
+      paper: colors.common.white,
+    },
+    primary: {
+      main: colors.blue[500],
+    },
+    secondary: {
+      main: colors.indigo[500],
+    },
+    text: {
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600],
+    },
+  },
+});
+
+export default theme;
