8815f43d07b67513d4db857939b866c4912e7082
Jamie Howells <jamie.howells@dmwgroup.co.uk>
2021-03-04 14:29:31 +0000

    GH-97 Add variable for API base URL

--- hybrid-frontend/plugins/assets-api-plugin/gatsby-node.js
+++ hybrid-frontend/plugins/assets-api-plugin/gatsby-node.js
@@ -1,12 +1,13 @@
-const fetch = require("node-fetch");
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
-const API_BASE_URL =
-  'https://digital-hybrid-mgmt.azure-api.net/digitalhybrid/Assets';
-const ASSET_NODE_TYPE = `Asset`;
+const fetch = require('node-fetch');
+
+const ASSET_NODE_TYPE = 'Asset';
 
 // print a success message if the plugin loads
-exports.onPreInit = () =>
-  console.log("\x1b[32m%s\x1b", "success loaded assets-api-plugin");
+exports.onPreInit = () => console.log('\x1b[32m%s\x1b', 'success loaded assets-api-plugin');
 
 exports.sourceNodes = async ({
   actions,
@@ -15,28 +16,24 @@ exports.sourceNodes = async ({
 }) => {
   const { createNode } = actions;
 
-  const response = await fetch(`${API_BASE_URL}/readymade/standingdata`);
+  const response = await fetch(`${process.env.ASSETS_API_BASE_URL}/readymade/standingdata`);
 
   if (!response.ok) {
-    console.error("🚨 Unable to fetch assets API data");
+    console.error('🚨 Unable to fetch assets API data');
     return;
   }
 
   const resultData = await response.json();
 
-  resultData.tilneyStandingData.forEach((asset) =>
-    createNode({
-      ...asset,
-      id: createNodeId(`${ASSET_NODE_TYPE}-${asset.id}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: ASSET_NODE_TYPE,
-        content: JSON.stringify(asset),
-        contentDigest: createContentDigest(asset),
-      },
-    })
-  );
-
-  return;
+  resultData.tilneyStandingData.forEach((asset) => createNode({
+    ...asset,
+    id: createNodeId(`${ASSET_NODE_TYPE}-${asset.id}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: ASSET_NODE_TYPE,
+      content: JSON.stringify(asset),
+      contentDigest: createContentDigest(asset),
+    },
+  }));
 };
