389b85d937b06099504c9ba24775de4c0c77dd38
Jamie Howells <jamie.howells@dmwgroup.co.uk>
2021-03-18 11:16:18 +0000

    DH-100 Capture goal component

---
+++ hybrid-frontend/src/components/CaptureGoal/CaptureGoal.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import CaptureGoal, { CaptureGoalProps } from './CaptureGoal';
+
+export default {
+  title: 'Digital Hybrid/Capture Goal',
+  component: CaptureGoal,
+  argTypes: { onSubmit: { action: 'submitted' } },
+} as Meta;
+
+const Template: Story<CaptureGoalProps> = (args) => (
+  <CaptureGoal
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...args}
+  />
+);
+
+export const Default = Template.bind({});
