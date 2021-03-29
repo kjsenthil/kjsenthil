76a6d9b72094dc389c96ef050f31cb6014dacd93
Issa <issashabo@gmail.com>
2021-03-29 14:00:37 +0100

    chore: convert gatsby config files to ts (#151)
    * chore: convert gatsby config files to ts
    
    * chore: add gatsby-*.ts to tsconfig

---
+++ hybrid-frontend/gatsby-config.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
+
+dotenv.config({
+  path: `.env.${activeEnv}`,
+});
+
+const env = activeEnv === 'production' ? 'prod' : 'dev';
+
+export default {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/src/data/${env}/features`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/img/`,
+      },
+    },
+
+    // Custom plugins
+    'assets-api-plugin',
+  ],
+};
