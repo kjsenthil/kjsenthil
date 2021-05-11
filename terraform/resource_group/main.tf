628d013eeb27b2c2d66d2f357f6eadd2ddf9aadc
Ben F <ben.fletcher@credera.co.uk>
2021-05-11 17:00:29 +0100

    Create artifact storage with resource group and use in pipeline (#8)
    * Create artifact storage with resource group and use in pipeline
    
    * Fix TF output to refer to array'd resources
    
    * Output null if no artifact resource is created

--- terraform/resource_group/main.tf
+++ terraform/resource_group/main.tf
@@ -3,3 +3,29 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
   tags     = merge(local.default_tags, var.tags)
 }
+
+resource "azurerm_storage_account" "artifact_storage" {
+  count = var.environment_prefix == "staging" ? 1 : var.environment_prefix == "prod" ? 1 : 0
+
+  name                = "st${local.short_location}tsw${var.environment_prefix}artifacts"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  account_kind             = "Storage"
+
+  tags = merge(var.tags, local.default_tags)
+}
+
+resource "azurerm_storage_container" "artifact_container" {
+  count = var.environment_prefix == "staging" ? 1 : var.environment_prefix == "prod" ? 1 : 0
+  name                 = var.environment_prefix
+  storage_account_name = azurerm_storage_account.artifact_storage[0].name
+}
+
+resource "azurerm_storage_container" "test_artifact_container" {
+  count = var.environment_prefix == "staging" ? 1 : var.environment_prefix == "prod" ? 1 : 0
+  name                 = "tests"
+  storage_account_name = azurerm_storage_account.artifact_storage[0].name
+}
--- terraform/resource_group/outputs.tf
+++ terraform/resource_group/outputs.tf
@@ -7,3 +7,18 @@ output "location" {
   value       = azurerm_resource_group.resource_group.location
   description = "The location in which this resource group is deployed."
 }
+
+output "artifact_storage_account_name" {
+  value       = length(azurerm_storage_account.artifact_storage) > 0 ? azurerm_storage_account.artifact_storage[0].name : null
+  description = "Name of the storage account used to house deployment artifacts"
+}
+
+output "artifact_container_name" {
+  value       = length(azurerm_storage_container.artifact_container) > 0 ? azurerm_storage_container.artifact_container[0].name : null
+  description = "Name of the container in the storage account used to house deployment artifacts"
+}
+
+output "test_artifacts_container_name" {
+  value       = length(azurerm_storage_container.test_artifact_container) > 0 ? azurerm_storage_container.test_artifact_container[0].name : null
+  description = "Name of the container in the storage account used to house test artifacts"
+}
