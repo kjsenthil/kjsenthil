20ce541aeeb885d841a54e21c5619b24d770baf8
Nick Griffiths <nicholas.griffiths@credera.co.uk>
2021-07-29 15:03:09 +0100

    Add jest styled components (#233)
    * chore: add jest-styled-components
    Generates consistent styled-component class names in tests, which makes snapshot tests less brittle. Previously, changes to code unrelated to the components under test would cause styled components to generate new class names, which caused snapshot tests to fail.
    
    * chore: add jest-styled-components import to global config
    
    * chore: Update snapshots for change from master

--- packages/hybrid-frontend/test-utils/setup-test-env.js
+++ packages/hybrid-frontend/test-utils/setup-test-env.js
@@ -1,6 +1,7 @@
 import { setTheme } from '@tsw/test-util';
 import '@testing-library/jest-dom/extend-expect';
 import theme from '../src/themes/mui';
+import 'jest-styled-components';
 
 jest.mock('../src/assets/fonts', () => ({}));
 jest.mock('../src/components/particles/Counter', () =>
